b8455c8a6d189371954f2636f6635994ed8efdd1
SteveLeong <Steven@Stevens-MBP.hitronhub.home>
2018-03-09 19:12:43 -0700

    cleaned up

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 let userList = [];
-
-
+let msgHistory = [];
 
 http.listen( port, function () {
     console.log('listening on port', port);
@@ -24,30 +23,41 @@ io.on('connection', function(socket){
     socket.on('chat', function(msg){
         let time = getTime();
 
+        let msgFormat = "<div>" + time + " <span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + ": " + msg + "</div>"
 
         if (msg.startsWith("/nick ")){
             let oldNick = socket.username;
             let newNick = msg.replace('/nick ', '');
+
             if(userList.includes(newNick)){
-                socket.emit('chat','This nickname is already be taken.');
+                socket.emit('username','This nickname is already be taken.');
             }else{
+                // Remove the old username from the list of online users
                 let index = userList.indexOf(socket.username);
                 if (index > -1) {
                     userList.splice(index, 1, newNick);
                 }
                 socket.username = newNick;
+                // Send message to sender
+                socket.emit('chat',("<b>" + time + " You have successfully changed your nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
+                // Send message to everyone else
+                socket.broadcast.emit('chat',("<b>" + time + " " + oldNick + " has changed their nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
+                // Update the list of online users
+                updateCurrUsers(userList);
             }
-            socket.emit('chat',("<b>" + time + " You have successfully changed your nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
-            socket.broadcast.emit('chat',("<b>" + time + " " + oldNick + " has changed their nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
-            updateCurrUsers(userList);
+            
         }else if(msg.startsWith("/nickcolor ")){
             let newColor = msg.replace('/nickcolor ', '');
             socket.color = "#" + newColor;
             socket.emit('chat',("<b>" + time + " You have successfully changed your nickname color to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
         }else {
-            socket.emit('chat',("<b>" + time + " <span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + ": " + msg + "</b>"));
-            socket.broadcast.emit('chat',("<div>" + time + " <span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + ": " + msg + "</div>"));
 
+            socket.emit('chat', ("<b>" + msgFormat + "</b>"));
+            socket.broadcast.emit('chat', msgFormat);
+            
+            //if (msgHistory.length <= 200){
+
+            //}
         }
     });
 
