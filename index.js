b51dfa362ce6a47ef38937347893635ca66ebeaa
SteveLeong <Steven@Stevens-MBP.hitronhub.home>
2018-03-10 15:47:58 -0700

    chat log history

--- index.js
+++ index.js
@@ -16,15 +16,26 @@ app.use(express.static(__dirname + '/public'));
 // listen to 'chat' messages
 io.on('connection', function(socket){
     socket.username = setUser();
-    socket.color = "#86c232"
+    socket.color = "#86c232"    // default color
     userList.push(socket.username);
     updateCurrUsers(userList);
+
+    /** CHAT LOG HISTORY 
+     *  History only remembers regular messages and nickname changes
+    */
+    for (let i=0; i<msgHistory.length; i++){
+        socket.emit('chat', msgHistory[i]);
+    }
+
+    // Welcome message for all new users
+    socket.emit('chat', ("<b> <span style=\"font-size: 20px;\"> Welcome To The Seng513 Chatroom! </span> </b>"))
     socket.emit('username', "Your username is " + socket.username)
+
     socket.on('chat', function(msg){
         let time = getTime();
-
         let msgFormat = "<div>" + time + " <span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + ": " + msg + "</div>"
 
+        /** CHANGE NICKNAME */
         if (msg.startsWith("/nick ")){
             let oldNick = socket.username;
             let newNick = msg.replace('/nick ', '');
@@ -41,37 +52,36 @@ io.on('connection', function(socket){
                 // Send message to sender
                 socket.emit('chat',("<b>" + time + " You have successfully changed your nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
                 // Send message to everyone else
-                socket.broadcast.emit('chat',("<b>" + time + " " + oldNick + " has changed their nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
+                let nickChangeMsg = time + " " + oldNick + " has changed their nickname to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "."
+                socket.broadcast.emit('chat', nickChangeMsg);
                 // Update the list of online users
                 updateCurrUsers(userList);
+                // Save to message history
+                updateHistory(nickChangeMsg);
             }
-            
-        }else if(msg.startsWith("/nickcolor ")){
+        /** CHANGE NICKNAME COLOR */
+        }else if(msg.startsWith("/nickcolor ")){ // If color does not exist it returns to default
             let newColor = msg.replace('/nickcolor ', '');
             socket.color = "#" + newColor;
             socket.emit('chat',("<b>" + time + " You have successfully changed your nickname color to " + "<span style=\"color: " + socket.color + ";\">" + socket.username + "</span>" + "." + "</b>"));
-        }else {
-
+        }else{  // Regular message
+            /** BOLD MESSAGES */
             socket.emit('chat', ("<b>" + msgFormat + "</b>"));
             socket.broadcast.emit('chat', msgFormat);
-            
-            //if (msgHistory.length <= 200){
-
-            //}
+            updateHistory(msgFormat);
         }
     });
 
+    // When a user disconnects their username is removed from the list of online users
     socket.on('disconnect', function(){
         let index = userList.indexOf(socket.username);
             if (index > -1) {
                 userList.splice(index, 1);
             }
     });
-
 });
 
-
-
+/** TIME STAMPS */
 function getTime(){
     let now = new Date();
     let timeFormat = [
@@ -84,6 +94,7 @@ function getTime(){
     return timeFormat;
 }
 
+/** Format for time stamps */
 function addZero(n){
     if (n < 10){
         n = "0" + n
@@ -91,6 +102,7 @@ function addZero(n){
     return n;
 }
 
+/** UNIQUE USERNAMES */
 function setUser(){
     do{
     let usernumber = Math.floor((Math.random() * 100) + 1);
@@ -99,10 +111,19 @@ function setUser(){
     return name;
 }
 
-function checkUsername(){
-
-}
-
+/** Send updated list of online users to all users */
 function updateCurrUsers(list){
     io.emit('userList', list);
+}
+
+/** Updates the Chat Log History
+ *  remembers at most the last 250 messages
+ */
+function updateHistory(msg){
+    if (msgHistory.length < 250){
+        msgHistory.push(msg);
+    }else{
+        msgHistory.shift();
+        msgHistory.push(msg);
+    }
 }
--- public/client.js
+++ public/client.js
@@ -11,27 +11,31 @@ $(function() {
     });
 
     socket.on('chat', function(msg){
-
         if(msg.startsWith('Your username is ')){
             myNick = msg.replace('Your username is ', '');
         }
-
+        /** SCROLL-UP TEXT */
         // https://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up
         var messages = document.getElementById('messages')
         var isScrolledToBottom = messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1;
         //console.log(messages.scrollHeight - messages.clientHeight,  messages.scrollTop + 1);
         $('#messages').append($('<li>' + msg));
-
-        
-        if(isScrolledToBottom)
+        if(isScrolledToBottom){
             messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+        }
 
     });
-
+    /** Displays username message from server */
     socket.on('username', function(name){
+        var messages = document.getElementById('messages')
+        var isScrolledToBottom = messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1;
         $('#messages').append($('<li>').text(name));
+        if(isScrolledToBottom){
+            messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+        }
     });
 
+    /** Displays list of current online users */
     socket.on('userList', function(list){
         $('#names > li').remove();
         for (let i=0; i<list.length; i++){
